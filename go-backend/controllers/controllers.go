5058950089ccba3c2c5bbafe33b1b5b9f8d9c3a5
Arpit Blagan <blaganarpit@gmail.com>
2025-01-03 09:48:07 +0530

    working on controllers

--- go-backend/controllers/controllers.go
+++ go-backend/controllers/controllers.go
@@ -8,6 +8,13 @@ import (
 	"net/http"
 )
 
+type createUser struct{
+	Name string `json:"name"`
+	Email string `json:"email"`
+	Password string `json:"password"`
+	Image string `json:"image"`
+}
+
 func HandleGetUsers(w http.ResponseWriter,r *http.Request){
 	var users []model.User
 		if(db.DB==nil){
@@ -31,4 +38,29 @@ func HandleGetUsers(w http.ResponseWriter,r *http.Request){
 		w.Write(data)
 }
 
-// Define other controllers here....
+func HandleRegisterUser(w http.ResponseWriter,r *http.Request){
+	//Extract the body from the request first
+	var user createUser
+	if err:=json.NewDecoder(r.Body).Decode(&user); err!=nil{
+		fmt.Println(err)
+		http.Error(w,"Error while decoding the req body :(",http.StatusInternalServerError)
+		return
+	}
+	defer r.Body.Close()
+	//Create user entry in DB
+	if err := db.DB.Create(&user).Error; err != nil {
+		// If database insertion fails, return an error
+		http.Error(w, "Failed to create user", http.StatusInternalServerError)
+		fmt.Println("Error while creating user in the database:", err)
+		return
+	}
+	data,err:=json.Marshal(user)
+	if(err!=nil){
+		http.Error(w,"Error while marshaling the user",http.StatusInternalServerError)
+		fmt.Println("Not able to convert the data into json format")
+		return
+	}
+	w.Header().Set("Content-Type","application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(data)
+}
